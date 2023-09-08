ae5b708ad944ae6ced9fd76d1d8d67e5f1e6e3bf
Matthew Baer <matt.baer77@gmail.com>
2023-09-07 23:51:37 -0400

    "Step Three" and "Step Four" completed

--- src/hooks.js
+++ src/hooks.js
@@ -1,4 +1,7 @@
 import React, { useState } from "react";
+import uuid from "uuid";
+import axios from "axios";
+
 
 // Accepts initiallyUp - true or false will determine if the default behavior will show front or back of card respectively
 
@@ -13,4 +16,18 @@ const useFlip = ( initiallyUp=true ) => {
 
 }
 
-export default useFlip;
+// https://deckofcardsapi.com/api/deck/new/draw/
+const useAxios = (baseUrl) => {
+
+    const [responses, setResponses] = useState([]);
+    const addResponse = async (endPointName) => {
+      const response = await axios.get( (baseUrl + endPointName + "/")
+      );
+      setResponses(responses => [...responses, { ...response.data, id: uuid() }]);
+    };
+
+    return [responses, addResponse]
+
+}
+
+export  {useFlip, useAxios};
